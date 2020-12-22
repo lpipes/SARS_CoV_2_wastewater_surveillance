d3bc549089f9d90bac24d2be6f400fe42ecfd109
Selina Kim <selinakim@berkeley.edu>
2020-12-21 17:06:52 -0800

    Fixed issue with columns

--- scripts/EM.py
+++ scripts/EM.py
@@ -14,10 +14,6 @@ output_mismatch_table_old = pd.read_csv(output_reads_filepath, sep="\t", header=
 readin_time = time.time() - t0
 print("Read in output mismatch reads", readin_time)
 
-<<<<<<< Updated upstream
-output_mismatch_table_old.rename(columns={output_mismatch_table_old.columns[0]:"read"}, inplace=True)
-eliminated = pd.read_csv(eliminated_filepath, sep="\n", header=0)
-=======
 output_mismatch_table.rename(columns={output_mismatch_table.columns[0]:"read"}, inplace=True)
 with open(eliminated_filepath, newline='') as f:
     reader = csv.reader(f, delimiter="\n")
@@ -26,7 +22,6 @@ with open(eliminated_filepath, newline='') as f:
 print(output_mismatch_table.size)
 print("col old: ", output_mismatch_table.columns)
 output_mismatch_table.drop(columns=eliminated)
->>>>>>> Stashed changes
 
 print(output_mismatch_table_old.size)
 print("col old: ", output_mismatch_table_old.columns)
@@ -45,28 +40,15 @@ ft.write_dataframe(SID, SID_path, compression='uncompressed')
 # Rename labels for names of strains
 n = len(output_mismatch_table.columns) - 1
 
-<<<<<<< Updated upstream
-#col_names = ["read", "blockSizes"]
 output_mismatch_table.rename(columns = {0 : "read", 1: "blockSizes"})
-#col_names.extend(np.arange(n - 1))
-#col_names = [str(n) for n in col_names]
-#output_mismatch_table.columns = col_names
-=======
-output_mismatch_table.rename(columns = {0 : "read", 1: "blockSizes"})
->>>>>>> Stashed changes
 reads_col = output_mismatch_table["read"]
 
 ## dfN
 dfN_col = output_mismatch_table["blockSizes"]
 dfN_col = pd.DataFrame(data=dfN_col)
 dfN = pd.concat([reads_col, pd.concat([dfN_col] * (n-1), axis=1, ignore_index=True)], axis=1)
-<<<<<<< Updated upstream
 col_names = output_mismatch_table.columns[1:n+1]
 col_names.insert(0, "read")
-=======
-col_names = output_mismatch_table.columns[2:n+1]
-col_names = col_names.insert(0, "read")
->>>>>>> Stashed changes
 dfN.columns = col_names
 print("col_names: ", col_names)
 
