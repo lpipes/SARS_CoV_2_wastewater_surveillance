804942d437493d928d536c68913e54bd0132f5b1
Edward-Chen <78003795+Edward-Z-Chen@users.noreply.github.com>
2021-03-17 05:24:33 -0800

    add codes to handle unidentifiability issue

--- EM.R
+++ EM.R
@@ -10,9 +10,11 @@ library(hash)
 library(feather)
 library(glue)
 library(arrow)
-library(daarem)
+library(turboEM)
 library(ggplot2)
 library(stringr)
+library(parallel)
+library(doParallel)
 
 
 args <- commandArgs(trailingOnly = TRUE)
@@ -27,13 +29,15 @@ if (!file.exists(args[1])) {
 
 # Read in data
 ptm <- proc.time()
-data <- read.table(args[1], header = T)
+data <- read.table(args[1], header = T,sep = "\t")
 cat("Loading matrices in from file", (proc.time() - ptm)[3])
 
 if (nrow(data)<20){
   print("Not enough reads!")
   quit()
 }
+
+args[1] <- gsub(".txt","",args[1],fixed = T)
 
 ptm <- proc.time()
 strains <- colnames(data[, 3:ncol(data)])
@@ -57,19 +61,24 @@ if (length(args) == 1) {
   quit()
 } else {
   error <- as.numeric(args[2])
+  if (error<0.001 | error >1) {
+    print("\nError rate should be smaller than 0.01 or larger than 1!")
+    quit()
+  }
 }
 ptm <- proc.time()
 Q <- ((error)^d_mtx) * ((1 - error)^(n_mtx - d_mtx))
+Q <- as.matrix(Q)
 cat("\nCreate Q-matrix", (proc.time() - ptm)[3])
 ptm <- proc.time()
 
-#-------------------------------------DAAREM---------------------
+#-------------------------------------SQUAREM---------------------
 # objective function
 logL <- function(p, y) {
   return(sum(log(colSums(t(y) * p))))
 }
-# #objective function for SQUAREM
-# neg.logL <- function(p, y) return((-logL(p,y)))
+#objective function for SQUAREM
+neg.logL <- function(p, y) return((-logL(p,y)))
 
 # Update Function
 EM <- function(p, y) {
@@ -87,41 +96,81 @@ round.output <- function(p) {
   p
 }
 
+# Combining unidentifiable strains
+ptm <- proc.time()
+loglikelihoods <- colSums(log(Q))
+p.one <- rep(0, k)
+p.one[which.max(loglikelihoods)] <- 1
+uni.like <- unique(loglikelihoods)
+if (length(uni.like)!=length(loglikelihoods)){
+  r_file = file(paste0("Unidentifiable Strains_",args[1],".txt"), "w")
+  unident <- 0
+  for (i in uni.like){
+    if (sum(loglikelihoods==i)>1){
+      tmp <- which(loglikelihoods==i)
+      #The first two column of d_mtx aren't mismatch number
+      d_tmp <- unique(as.matrix(d_mtx[,tmp]),MARGIN = 2)
+      if (ncol(d_tmp)<length(tmp)){
+        for (j in 1:ncol(d_tmp)){
+          unident.names <- colnames(d_mtx[,tmp])[apply(d_mtx[,tmp], 2, identical,y=d_tmp[,j])]
+          if (length(unident.names)>1){
+            unident <- unident+1
+            write(paste0("Group ",unident,":"),file = r_file,append = T)
+            write(unident.names,file = r_file,append = T)
+            Q.index <- which(colnames(Q) %in% unident.names)
+            colnames(Q)[Q.index[1]] <- paste0("Group ",unident)
+            Q <- as.matrix(Q[,-Q.index[-1]])
+          }
+        }
+      }
+    }
+  }
+  close(r_file)
+  if(ncol(Q)==1){
+    colnames(Q) <- "Group 1"
+  }
+}
+cat("\nTime cost for finding unidentifiable strains: ",(proc.time()-ptm)[3])
+
 # EM Algorithm
-p0 <- runif(k)
-p0 <- p0 / sum(p0)
-res <- daarem(p0,
-  fixptfn = EM, objfn = logL, y = Q,
-  control = list(tol = 1e-07, convtype = "objfn", maxiter = 200000)
-)
-p <- res$par
+p0 = colSums(Q)
+p0 = p0 / sum(p0)
+options(digits=13)
+
+# cl.cores = detectCores(logical = F)
+# cl <- makeCluster(cl.cores)
+# registerDoParallel(cl,cores = cl.cores)
+
+res = turboem(par=p0, fixptfn=EM, objfn=neg.logL, method="squarem", y=Q, parallel = F,
+              control.run = list(convtype = "objfn",tol = 1.0e-7,
+                                 stoptype = "maxtime",maxtime = 10000))
+
+p <- pars(res)
 p <- round.output(p)
-cat("\nDAAREM algorithm ", (proc.time() - ptm)[3])
+
+# stopImplicitCluster()
+cat("\nSQUAREM algorithm ", (proc.time() - ptm)[3])
 
 # One Strain
-loglikelihoods <- apply(diag(k), 1, logL, y = Q)
-cat("\nAssuming only one strain: ", colnames(n_mtx)[which.max(loglikelihoods)])
+loglikelihoods <- colSums(log(Q))
+cat("\nAssuming only one strain: ", colnames(n_mtx)[loglikelihoods==max(loglikelihoods)],sep = "\n")
 p.one <- rep(0, k)
 p.one[which.max(loglikelihoods)] <- 1
 cat("\nlog(Likelihood of multiple strains/Likelihood of one strain): ", logL(p, Q) - logL(p.one, Q),"\n")
 
 par(mar = c(13, 4, 4, 1))
-names.arg <- colnames(n_mtx) # paste0(tolower(names(SID)[as.numeric(colnames(n_mtx))]), ", ", 1:k)
-if (length(names.arg) != length(p)) {
-  names.arg <- colnames(d_mtx)
-}
+names.arg <- colnames(Q)
 em_output_df <- cbind(p, names.arg)
-em_output_df <- as.data.frame(em_output_df)
+em_output_df <- data.frame(em_output_df)
 colnames(em_output_df) <- c("p", "names.arg")
 
 write.csv(em_output_df, file = paste("./em_output_", args[1], ".csv", sep = ""))
-reads_caset10_30000_423_.005.txt
 
 # d <- data.table(em_output_df, key="p")
 d <- data.frame(p, names.arg)
 d <- d[order(d$p, decreasing = T), ]
-d <- head(d, 50)
-d <- d[d$p != 0, ]
+d <- d[d$p > 0.01, ]
+d <- head(d, 30)
 
 pdf(file = paste("./proportion_plot_", args[1], ".pdf", sep = ""), width = 15, height = 10)
 ggplot(data = d, mapping = aes(x = p, y = reorder(names.arg, p))) +
