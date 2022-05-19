a362db9e446031c5bbf0180e4655986228c715ce
lpipes <lenore@tilden.biol.berkeley.edu>
2022-05-19 10:53:54 -0700

    changed path to EM code

--- eliminate_strains/eliminate_strains.c
+++ eliminate_strains/eliminate_strains.c
@@ -1978,9 +1978,9 @@ int main(int argc, char **argv){
 	buffer = (char*)malloc(FASTA_MAXLINE*sizeof(char));
 	memset(buffer,'\0',FASTA_MAXLINE);
 	if (opt.llr==1){
-		sprintf(buffer,"Rscript /ocean/projects/bio180015p/pipes/ww_benchmark/samples/EM_C_LLR_updated.R -i %s -f %lf -e %lf -l -s -v %s -r %s -b %s",opt.outfile,opt.freq,opt.error,opt.variant,opt.fasta,opt.print_counts);
+		sprintf(buffer,"Rscript EM_C_LLR_updated.R -i %s -f %lf -e %lf -l -s -v %s -r %s -b %s",opt.outfile,opt.freq,opt.error,opt.variant,opt.fasta,opt.print_counts);
 	}else{
-		sprintf(buffer,"Rscript /ocean/projects/bio180015p/pipes/ww_benchmark/samples/EM_C_LLR_updated.R -i %s -f %lf -e %lf -s -v %s -r %s -b %s",opt.outfile,opt.freq,opt.error,opt.variant,opt.fasta,opt.print_counts);
+		sprintf(buffer,"Rscript EM_C_LLR_updated.R -i %s -f %lf -e %lf -s -v %s -r %s -b %s",opt.outfile,opt.freq,opt.error,opt.variant,opt.fasta,opt.print_counts);
 	}
 	system(buffer);
 	free(buffer);
